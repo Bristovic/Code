d05f34f09590e133c9d17a0a22caface6525df0c
Bristovic <adambristowsmith@outlook.com>
2026-01-29 18:48:07 +0100

    First C++ program.

---
+++ hellogoodbye.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//main: generate some simple output
+
+int main ()
+{
+    cout << "Hello, world." << endl;
+    cout << "Goodbye, ";
+    cout << "Cruel World!!!"<<endl;
+    return 0;   
+}
